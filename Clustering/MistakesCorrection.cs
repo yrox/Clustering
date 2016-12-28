1968df13c55dd4f882bb5b3752d329a7472ee387
Olga Yaroshchuk <yrox152@gmail.com>
2016-12-28 15:45:33 +0300

    finding most frequent element in cluster

--- Clustering/MistakesCorrection.cs
+++ Clustering/MistakesCorrection.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
-using Clustering.Algorythms;
+using System.Linq;
+using Clustering.Interfaces;
 using Clustering.IO;
+using Clustering.NinjectBindings;
+using Ninject;
 
 namespace Clustering
 {
@@ -15,23 +18,29 @@ namespace Clustering
         private Clustering _clustering;
         private CmdOptions _options;
 
+        private string GetDefaultClusterElement(IEnumerable<int> linesIndexes)
+        {
+            var columnIndex = _initialTable.Colunms.IndexOf(_options.ColumnName);
+            var values = linesIndexes.Select(index => _initialTable.table.ElementAt(index).ElementAt(columnIndex)).ToList();
+            return values.GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).First();
+        }
+
         private void FixTable(IDictionary<string, IList<int>> clusters)
         {
             _initialTable.CorrectColumn(_options.ColumnName, clusters);
         }
 
-        public Table CorrectMistakes(Table table)
+        public Table CorrectMistakes(Table table, string algName)
         {
             _clustering = new Clustering();
             _initialTable = table;
             var columnName = _options.ColumnName;
 
-            FixTable(_clustering.GetClustersDictionary(new KeyCollision(), columnName, _initialTable));
-            FixTable(_clustering.GetClustersDictionary(new NGram(_options.IntArg), columnName, _initialTable));
-            FixTable(_clustering.GetClustersDictionary(new Compression(_options.DoubleArg), columnName, _initialTable));
-            FixTable(_clustering.GetClustersDictionary(new Levenshtein(_options.IntArg), columnName, _initialTable));
-            FixTable(_clustering.GetClustersDictionary(new PhoneticSimilarity(), columnName, _initialTable));
+            var kernel = new StandardKernel(new ClusteringAlgBindings(_options));
+            var alg = kernel.Get<IClusteringAlg>(algName);
 
+            FixTable(_clustering.GetClustersDictionary(alg, columnName, _initialTable));
+            
             return _initialTable;
         }
     }
--- Clustering/Program.cs
+++ Clustering/Program.cs
@@ -1,4 +1,5 @@
-﻿using Clustering.NinjectBindings;
+﻿using System.Collections.Generic;
+using Clustering.NinjectBindings;
 using Clustering.Interfaces;
 using Clustering.IO;
 using CommandLine;
@@ -11,16 +12,17 @@ namespace Clustering
         static void Main(string[] args)
         {
             var options = new CmdOptions();
+            var corrector = new MistakesCorrection(options);
             Parser.Default.ParseArguments(args, options);
              
             var tr = new TableReader();
             var tw = new TableWriter();
             var t = new Table(tr.Read(options.InputFile));
             var clust = new Clustering();
-            var corrector = new MistakesCorrection(options);
+            
 
-            var kernel = new StandardKernel(new ClusteringAlgBindings(options));
-            var alg = kernel.Get<IClusteringAlg>(options.Algorythm);
+            //var kernel = new StandardKernel(new ClusteringAlgBindings(options));
+            //var alg = kernel.Get<IClusteringAlg>(options.Algorythm);
 
             //var algType = Type.GetType("Clustering.Algorythms." + options.Algorythm + ", Clustering");
             //var alg = (IClusteringAlg)Activator.CreateInstance(algType, options.DoubleArg, options.IntArg);
@@ -28,7 +30,7 @@ namespace Clustering
             //var a = Activator.CreateInstance("Clustering", "Clustering.Options");
             //var alg = Activator.CreateInstance("Clustering", options.Algorythm) as IClusteringAlg;
 
-            tw.Write(corrector.CorrectMistakes(t));
+            tw.Write(corrector.CorrectMistakes(t, "KeyCollision"));
             //tw.Write(clust.GetClusters(alg, options.ColumnName, t), options.ColumnName);
         }
     }
